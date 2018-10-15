70344036f48a448f15544ea5f25c1ebbcfbb972e
mohan <mohandeblaze@gmail.com>
2018-10-15 18:26:54 +0530

    v1.0.23

--- bin/module_remover.js
+++ bin/module_remover.js
@@ -6,6 +6,6 @@ var moduleRemover = require(path.resolve(__dirname, '../src/index'));
 var argsStripped = process.argv.splice(process.execArgv.length + 2);
 
 var args = argsStripped[0];
-console.log(args ? `User given path is taken ${args}` :
+console.log(args ? `Path -> ${args}` :
     `Path -> ${process.cwd()}${path.sep}node_modules`);
 moduleRemover(args ? args : `${process.cwd()}${path.sep}node_modules`);
