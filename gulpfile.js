579fc64169a022d4e58cfb1b8945c2518c41316f
Mohan R <mohandeblaze@gmail.com>
2018-12-06 17:45:19 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@ const fsExtra = require('fs-extra');
 const shell = require('shelljs');
 
 gulp.task('publish', function () {
-    var packageJson = fsExtra.readJsonSync('package.json');
+    var packageJson = fsExtra.readJsonSync('./package.json');
     var version = parseInt(packageJson.version.split('.')[2]);
+    console.log(`Previous version is ${version.toString()}`);
     version = (++version).toString();
+    console.log(`Current version is ${version.toString()}`);
     packageJson.version = packageJson.version.split('.')[0] + '.' + packageJson.version.split('.')[1] + '.' +
         version;
     fsExtra.writeJsonSync('package.json', packageJson, {
